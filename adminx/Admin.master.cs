26fb9252fc2c6e19c9719ae0b4e896ff576beb39
nguyenductuan1982 <nguyenductuan1982@1d3001f8-4b54-0173-05a0-9f14055206c4>
2012-06-29 15:40:56 +0000

    Admin Page quay ve Co che cu...:( phan quyen...

--- adminx/Admin.master.cs
+++ adminx/Admin.master.cs
@@ -12,7 +12,7 @@ public partial class CMS_CMS_5B : System.Web.UI.MasterPage
     {
         if (!IsPostBack)
         {
-            //BuildMenu();
+            BuildMenu();
             if(Session["UserID"] != null)
             {
                 int PK_iUser = int.Parse(Session["UserID"].ToString());
@@ -35,34 +35,34 @@ public partial class CMS_CMS_5B : System.Web.UI.MasterPage
         Session.Remove("UserID");
         Response.Redirect("../");
     }
-    //private void BuildMenu()
-    //{
-    //    int GroupID = Convert.ToInt16(Session["GroupID"].ToString());
-    //    //List<GroupPermissionEntity> lstper = LoadAllPer(Convert.ToInt32(Session["UserID"]));
-    //    List<GroupPermissionEntity> lstper = GroupPermissionBRL.GetByiGroupID(GroupID);
-    //    foreach (Control cl in this.form1.Controls)
-    //    {
-    //        String[] s = cl.ID.Split('_');
-    //        if(s.Length>1)
-    //            cl.Visible = SetVisible(lstper,Convert.ToInt16(s[1]));
-    //    }
-    //}
-    //private bool SetVisible(List<GroupPermissionEntity> lstGP, int perID)
-    //{
-    //    int i = 0;
-    //    lstGP.ForEach(
-    //            delegate(GroupPermissionEntity oGP)
-    //            {
-    //                if (oGP.iPermissionID == perID)
-    //                {
-    //                    i = 1;                        
-    //                }
-    //            }
-    //        );
-    //    if (i == 1)
-    //        return true;
-    //    else
-    //        return false;
-    //}
+    private void BuildMenu()
+    {
+        int GroupID = Convert.ToInt16(Session["GroupID"].ToString());
+        //List<GroupPermissionEntity> lstper = LoadAllPer(Convert.ToInt32(Session["UserID"]));
+        List<GroupPermissionEntity> lstper = GroupPermissionBRL.GetByiGroupID(GroupID);
+        foreach (Control cl in this.form1.Controls)
+        {
+            String[] s = cl.ID.Split('_');
+            if (s.Length > 1)
+                cl.Visible = SetVisible(lstper, Convert.ToInt16(s[1]));
+        }
+    }
+    private bool SetVisible(List<GroupPermissionEntity> lstGP, int perID)
+    {
+        int i = 0;
+        lstGP.ForEach(
+                delegate(GroupPermissionEntity oGP)
+                {
+                    if (oGP.iPermissionID == perID)
+                    {
+                        i = 1;
+                    }
+                }
+            );
+        if (i == 1)
+            return true;
+        else
+            return false;
+    }
     
 }
